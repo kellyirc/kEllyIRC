6dc0253c4f090dc386649e82af67949963053bc2
imnothere199 <imnothere199@633d2ad2-4514-0e52-48b9-3540f28f37cc>
2011-01-04 18:02:29 +0000

    Began work on scripting.
    git-svn-id: https://kellyirc.googlecode.com/svn/trunk@27 633d2ad2-4514-0e52-48b9-3540f28f37cc

---
+++ src/scripting/Script.java
@@ -0,0 +1,12 @@
+package scripting;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class Script {
+	String name;
+	File reference;
+	String language;
+	ArrayList<String> functions;
+	ArrayList<String> tags;
+}
