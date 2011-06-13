83ec7c77f61da58c7ed7d9adcdd43421dd9959d5
imnothere199 <imnothere199@633d2ad2-4514-0e52-48b9-3540f28f37cc>
2011-06-13 21:24:37 +0000

    Updating scripts.
    git-svn-id: https://kellyirc.googlecode.com/svn/trunk@76 633d2ad2-4514-0e52-48b9-3540f28f37cc

--- scripts/aliases.js
+++ scripts/aliases.js
@@ -1,5 +1,5 @@
 
-
+//join a channel (password optional)
 function join(connection, args) {
 	
 	if(util.checkArgs(args, 1)) {
@@ -12,9 +12,35 @@ function join(connection, args) {
 
 }
 
+//used by join()
 function cleanChanStr(string) {
 	if(!string.startsWith("#")) {
 		return "#"+string;
 	}
 	return string;
+}
+
+//make an action
+function action(connection, args) {
+	connection.sendAction(global.currentChannel(), args);
+}
+
+//alias of an alias, woo!
+function me(connection, args) { action(connection, args); }
+
+//send a message to all channels on the given connection
+function amsg(connection, args) {
+	var channels = connection.getChannels().toArray();
+	for(var c in channels) {
+		connection.sendMessage(channels[c], args);
+	}
+}
+
+//send a message to something or someone or somewhere, somehow
+function msg(connection, args) {
+
+	if(util.checkArgs(args,2)) {
+		var args = util.getArgs(args,2);
+		connection.sendMessage(args[0], args[1]);
+	}
 }
