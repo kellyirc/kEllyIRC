f5b06ac0bc7ccdb7cad351ddcd44ac05b2cfa258
firefreek@gmail.com <firefreek@gmail.com@633d2ad2-4514-0e52-48b9-3540f28f37cc>
2012-01-23 02:31:25 +0000

    Info button for timestamp format
    git-svn-id: https://kellyirc.googlecode.com/svn/trunk@170 633d2ad2-4514-0e52-48b9-3540f28f37cc

--- src/ui/composites/TextBoxComposite.java
+++ src/ui/composites/TextBoxComposite.java
@@ -1,8 +1,11 @@
 package ui.composites;
 
+import hexapixel.cache.ImageCache;
+
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Text;
@@ -11,6 +14,9 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 
 import connection.Settings;
+import org.eclipse.swt.widgets.Group;
+
+import shared.NSAlertBox;
 
 public class TextBoxComposite extends Composite
 {
@@ -24,10 +30,11 @@ public class TextBoxComposite extends Composite
 	public TextBoxComposite(Composite parent, int style)
 	{
 		super(parent, style);
-		setLayout(new GridLayout(3, false));
+		setLayout(new GridLayout(4, false));
 		
 		Label lblTimestamp = new Label(this, SWT.NONE);
 		lblTimestamp.setText("Timestamps");
+		new Label(this, SWT.NONE);
 		new Label(this, SWT.NONE);
 		new Label(this, SWT.NONE);
 		
@@ -36,13 +43,41 @@ public class TextBoxComposite extends Composite
 		btnEnableTimestamps.setSelection(Settings.getSettings().isTimestampsEnabled());
 		new Label(this, SWT.NONE);
 		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
 		
 		Label lblTimestampFormat = new Label(this, SWT.NONE);
 		lblTimestampFormat.setText("Timestamp format: ");
 		
 		timestampFormatText = new Text(this, SWT.BORDER);
-		timestampFormatText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		GridData gd_timestampFormatText = new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1);
+		gd_timestampFormatText.widthHint = 97;
+		timestampFormatText.setLayoutData(gd_timestampFormatText);
 		timestampFormatText.setText(Settings.getSettings().getTimestampFormatPattern());
+		
+		Button btnTimestampHelp = new Button(this, SWT.NONE);
+		btnTimestampHelp.setImage(ImageCache.getImage("info.png"));
+		GridData gd_btnNewButton = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		gd_btnNewButton.widthHint = 37;
+		btnTimestampHelp.setLayoutData(gd_btnNewButton);
+		btnTimestampHelp.addSelectionListener(new SelectionAdapter(){
+			@Override
+			public void widgetSelected(SelectionEvent e)
+			{
+				super.widgetSelected(e);
+				NSAlertBox infoAlert = new NSAlertBox("Timestamp Formatting","The formatting is based on the SimpleDateFormat in Java. Here are some common pattern letters:\n\nY = Year\nM = Month in year\nd = Day in month\nE = Day name in week\na = Am/pmmarker\nH = Hour in day(0-23)\nh = Hour in am/pm(1-12)\nm = Minute in hour\ns = Second inminute\nS = Millisecond\n\nFor more information, check http://docs.oracle.com/javase/7/docs/api/java/text/SimpleDateFormat.html",SWT.ICON_QUESTION|SWT.OK);
+				infoAlert.go();
+				
+			}
+		});
+		
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
+		new Label(this, SWT.NONE);
 		new Label(this, SWT.NONE);
 		new Label(this, SWT.NONE);
 		new Label(this, SWT.NONE);
